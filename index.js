2c01a365820af0a0e9cbd20bbd2243e3314d7863
Thomas Steven <thomaspatricksteven@gmail.com>
2021-02-06 21:40:32 -0500

    initialize repo

---
+++ index.js
@@ -0,0 +1,81 @@
+const screenWidth = `${window.innerWidth * 0.8}px`;
+const screenHeight = `${window.innerHeight * 0.8}px`;
+
+const loader = document.getElementById("loader");
+const form = document.getElementById("form");
+const button = document.getElementById("button");
+const message = document.getElementById("message");
+const flattery = document.getElementById("flattery");
+const video = document.querySelector("video");
+
+video.style.width = screenWidth;
+video.style.height = screenHeight;
+video.onloadedmetadata = (e) => video.play();
+flattery.style.width = screenWidth;
+flattery.style.height = screenHeight;
+
+async function onFormSubmitGorgeous(event) {
+  event.preventDefault();
+  button.disabled = true;
+  video.classList.remove("hidden");
+  flattery.classList.remove("hidden");
+
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const tomsCamera = devices.find((device) => device.label.includes("C922"));
+
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    console.log(devices);
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { deviceId: tomsCamera.deviceId },
+    });
+
+    setTimeout(() => {
+      const utterance = new SpeechSynthesisUtterance();
+      utterance.text = "You look gorgeous!";
+      flattery.innerHTML = utterance.text;
+      window.speechSynthesis.speak(utterance);
+    }, 1000);
+
+    setTimeout(() => {
+      const utterance = new SpeechSynthesisUtterance();
+      utterance.text = "Do you work out?";
+      flattery.innerHTML = utterance.text;
+      window.speechSynthesis.speak(utterance);
+    }, 7000);
+
+    setTimeout(() => {
+      const utterance = new SpeechSynthesisUtterance();
+      utterance.text = "I love your hair.";
+      flattery.innerHTML = utterance.text;
+      window.speechSynthesis.speak(utterance);
+    }, 17000);
+
+    video.srcObject = stream;
+    setTimeout(() => {
+      video.classList.add("hidden");
+      flattery.classList.add("hidden");
+      flattery.innerHTML = "";
+      button.disabled = false;
+      stream.getTracks()[0].stop();
+      message.innerHTML = "Done!";
+    }, 20000);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  button.disabled = true;
+  loader.classList.remove("hidden");
+  setTimeout(() => {
+    loader.classList.add("hidden");
+    message.innerHTML = "Done!";
+    button.disabled = false;
+  }, 10000);
+}
+
+form.addEventListener("submit", onFormSubmitGorgeous);
